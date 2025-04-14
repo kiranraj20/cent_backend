3bffb7560b051c9cfcde70559ebd8c73b171e151
kiranraj20 <kiranraj80555@gmail.com>
2025-04-14 09:57:47 +0530

    .

--- routes/api.js
+++ routes/api.js
@@ -10,7 +10,7 @@ router.get("/events", async (req, res) => {
     const events = await Event.find();
     res.json(events);
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ message: err.message });  
   }
 });
 

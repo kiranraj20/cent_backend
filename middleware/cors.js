b52ef8f5ff75f96cee8404c467677dca8c6f6841
kiranraj20 <kiranraj80555@gmail.com>
2025-04-14 09:54:05 +0530

    .

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,4 +1,4 @@
-const allowedOrigins = ['https://cent-frontend.vercel.app/', 'http://localhost:3000'];
+const allowedOrigins = ['https://cent-frontend.vercel.app', 'http://localhost:3000'];
 
 const corsMiddleware = function(req, res, next) {
     const origin = req.headers.origin;
